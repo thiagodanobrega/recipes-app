ba75e98df72b7c0a55b0d25f9b7859e8e3148246
(GJ) Gabriel Julio <89492349+GJTrybe@users.noreply.github.com>
2022-03-28 18:02:45 -0300

    Componente Loading
    Cria componente Loading usando SVG

---
+++ src/components/loading/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carregando from '../../images/loading.svg';
+
+const Loading = () => (
+  <>
+    <Carregando height={ 30 } width={ 30 } />
+    {' '}
+    Carregando...
+  </>
+
+);
+
+export default Loading;
