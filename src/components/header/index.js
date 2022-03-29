48ef18b102975327df42ee9df96da09b525a10b9
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 15:32:19 -0300

    Feat: retifica o import dos svgs do header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import imgageProfile from '../../images/profileIcon.svg';
+import imgageSearch from '../../images/searchIcon.svg';
 
-const Header = () => (
+const Header = () => {
+  const [showSearch, setShowSearch] = useState(true);
 
-  <header>
-    <div>
-      <button
-        type="button"
-        name="profile-top-btn"
-      >
-        <Link to="/profile">Profile</Link>
-        <img
-          src="src/images/profileIcon.svg"
-          alt="desenho de uma silhueta humana"
-          data-testid="profile-top-btn"
+  return (
+
+    <header>
+      <form>
+        <button
+          type="button"
+          name="profile-top-btn"
+        >
+          <Link to="/profile">Profile</Link>
+          <img
+            src={ imgageProfile }
+            alt="desenho de uma silhueta humana"
+            data-testid="profile-top-btn"
+          />
+        </button>
+
+        <h1 data-testid="page-title">Foods</h1>
+
+        <button
+          type="button"
+          data-testid="search-top-btn"
+          onClick={ () => setShowSearch(!showSearch) }
+
+        >
+          <img src={ imgageSearch } alt="desenho de uma lupa" />
+        </button>
+        <input
+          type="text"
+          name="searchInput"
+          data-testid="search-input"
+          hidden={ showSearch }
         />
-      </button>
-
-      <h1 data-testid="page-title">Foods</h1>
-
-      <button
-        type="button"
-        data-testid="search-top-btn"
-      >
-        <img src="src/images/searchIcon.svg" alt="desenho de uma lupa" />
-      </button>
-    </div>
-  </header>
-);
+      </form>
+    </header>
+  );
+};
 
 export default Header;
