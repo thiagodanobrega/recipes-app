4d927bc9d1890f22b8b592dbfc3d5641ecb8db71
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 13:13:50 -0300

    test: mergeando com a branch remota. Aceitando as atualizações da remota.

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,11 @@
-/* import React from 'react';
+ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('Farewell, front-end', () => {
-<<<<<<< HEAD
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-}); */
-=======
   render(<App />)
   // const { getByText } = render(<App />);
   // const linkElement = getByText(/TRYBE/i);
   // expect(linkElement).toBeInTheDocument();
 });
->>>>>>> main-group-20
+
