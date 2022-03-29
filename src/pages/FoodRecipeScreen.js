7fe2b42ff6f63b7736f359ccf28ef5a229dec627
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 14:50:08 -0300

    Feat : adiciona Header ao FoodRecipeScreen

--- src/pages/FoodRecipeScreen.js
+++ src/pages/FoodRecipeScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/header';
 
 function FoodRecipeScreen() {
   return (
     <div>
-      <p>Tela principal de receitas de comidas</p>
+      <Header />
     </div>
   );
 }
