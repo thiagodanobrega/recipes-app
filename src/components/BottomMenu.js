d4abf750f57f08a764ec9a6bd26e449e90735661
Thiago Nobrega <thiagons@live.com>
2022-03-29 10:57:27 -0300

    :sparkles: feat: criando elementos do menu

---
+++ src/components/BottomMenu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function BottomMenu() {
+  return (
+    <footer data-testid="footer">
+      <NavLink
+        to="/drinks"
+        data-testid="drinks-bottom-btn"
+      >
+        <img src={ drinkIcon } alt="Ícone de bebida" />
+      </NavLink>
+      <NavLink
+        to="/explore"
+        data-testid="explore-bottom-btn"
+      >
+        <img src={ exploreIcon } alt="Ícone de explorar" />
+      </NavLink>
+      <NavLink
+        to="/foods"
+        data-testid="food-bottom-btn"
+      >
+        <img src={ mealIcon } alt="Ícone de explorar" />
+      </NavLink>
+    </footer>
+  );
+}
+
+export default BottomMenu;
