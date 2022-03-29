43f84b350f4795f55bc816ca103a1f254efcf674
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 12:13:51 -0300

    Feat: cria const context no myprovider

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import MyContext from './myContext';
 
@@ -5,8 +6,13 @@ function Provider({ children }) {
   const INITIAL_STATE = { nome: 'Xablau', idade: 100 };
   const [state, setState] = useState(INITIAL_STATE);
 
+  const contex = {
+    state,
+    setState,
+  };
+
   return (
-    <MyContext.Provider value={ state }>
+    <MyContext.Provider value={ contex }>
       {children}
     </MyContext.Provider>
   );
