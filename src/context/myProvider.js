c399325a984f61878fca64dbd095ec38f8e1e9f3
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 13:16:20 -0300

    Feat : aceitando mudanças feitas na branch local

--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,28 +1,16 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import MyContext from './myContext';
 
 function Provider({ children }) {
   const INITIAL_STATE = { nome: 'Xablau', idade: 100 };
   const [state, setState] = useState(INITIAL_STATE);
-<<<<<<< HEAD
-
-  const contex = {
-    state,
-    setState,
-  };
-
-  return (
-    <MyContext.Provider value={ contex }>
-=======
   const contextValue = {
     state,
     setState,
   };
   return (
     <MyContext.Provider value={ contextValue }>
->>>>>>> main-group-20
       {children}
     </MyContext.Provider>
   );
