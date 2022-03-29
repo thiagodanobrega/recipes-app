e00b28880b00750f4d5edaa9f37d2a6450574d27
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 12:15:27 -0300

    test: cria pasta, arquivo header.test e incia os tests do header

--- src/testHeader/Header.test.js
+++ src/testHeader/Header.test.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
-import Header from '../components/Header';
+import Header from '../components/header';
 
 test('Farewell, front-end', () => {
   render(<App />);
 });
 
 describe('Monte um component Header', () => {
-  it('9- Implemente os elementos do header na tela principal de receitas', () => {
+  test('9- Implemente os elementos do header na tela principal de receitas', () => {
     render(<Header />);
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const titlePage = screen.getByTestId('page-title');
+    const searchTopButton = screen.getByTestId('search-top-btn');
+
+    expect(profileIcon).toBeInTheDocument();
+    expect(titlePage).toBeInTheDocument();
+    expect(searchTopButton).toBeInTheDocument();
+  });
+
+  test('10-Implemente um ícone para a tela de perfil', () => {
+    render(<Header />);
+    const profile = screen.getByRole('img', { name: /desenho de uma silhueta humana/i });
+
+    expect(profile).toBeInTheDocument();
+  });
+
+  test('10.2-Implemente um título', () => {
+    render(<Header />);
+    const titleHeader = screen.getByRole('heading', { name: /foods/i });
+
+    expect(titleHeader).toBeInTheDocument();
+  });
+
+  test('10.3-Implemente um título', () => {
+    render(<Header />);
+    const searchIcon = screen.getByRole('img', { name: /desenho de uma lupa/i });
+
+    expect(searchIcon).toBeInTheDocument();
   });
 });
