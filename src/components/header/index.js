92c0f495f186795eda7d16a82006921861a1456a
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 17:46:06 -0300

    feat: adiciona radio buttons e button search

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,9 +7,8 @@ const Header = () => {
   const [showSearch, setShowSearch] = useState(true);
 
   return (
-
-    <header>
-      <form>
+    <>
+      <header>
         <button
           type="button"
           name="profile-top-btn"
@@ -21,9 +20,7 @@ const Header = () => {
             data-testid="profile-top-btn"
           />
         </button>
-
         <h1 data-testid="page-title">Foods</h1>
-
         <button
           type="button"
           data-testid="search-top-btn"
@@ -32,14 +29,54 @@ const Header = () => {
         >
           <img src={ imgageSearch } alt="desenho de uma lupa" />
         </button>
-        <input
-          type="text"
-          name="searchInput"
-          data-testid="search-input"
-          hidden={ showSearch }
-        />
-      </form>
-    </header>
+      </header>
+      <div>
+        <form>
+          <label htmlFor="ingredient">
+            Ingredient
+            <input
+              type="radio"
+              id="ingredient"
+              data-testid="ingredient-search-radio"
+              name="search"
+              value="ingredient"
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="radio"
+              id="name"
+              data-testid="name-search-radio"
+              name="search"
+              value="name"
+            />
+          </label>
+          <label htmlFor="firstLetter">
+            First Letter
+            <input
+              type="radio"
+              id="firstLetter"
+              data-testid="first-letter-search-radio"
+              name="search"
+              value="firstLetter"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="exec-search-btn"
+          >
+            Search
+          </button>
+          <input
+            type="text"
+            name="searchInput"
+            data-testid="search-input"
+            hidden={ showSearch }
+          />
+        </form>
+      </div>
+    </>
   );
 };
 
