63881342077a35d83b96c72394f09ad539c31e0f
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-03-29 22:11:14 -0300

    :construction: Feat: campo de Email, senha e botão

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginScreen() {
   return (
     <div>
-      <p>Tela de Login</p>
+      <input
+        type="email"
+        placeholder="Email"
+        data-testid="email-input"
+        onChange={ (event) => setEmail(event.target.value) }
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        data-testid="password-input"
+        onChange={ (event) => setPassword(event.target.value) }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ disabledButton() }
+        onClick={ enableButton }
+      >
+        Entar
+      </button>
     </div>
   );
 }
