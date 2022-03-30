92b8f50b668033edd01ff27bed7720dda56cf394
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 22:51:31 -0300

    feat : reestrutura o component header para renderizar apenas a imagem profile + redirecionamento

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,81 +1,24 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import imgageProfile from '../../images/profileIcon.svg';
-import imgageSearch from '../../images/searchIcon.svg';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../../images/profileIcon.svg';
 
 const Header = () => {
-  const [showSearch, setShowSearch] = useState(true);
-
+  const history = useHistory();
   return (
     <>
       <header>
-        <button
-          type="button"
-          name="profile-top-btn"
-        >
-          <Link to="/profile">Profile</Link>
-          <img
-            src={ imgageProfile }
-            alt="desenho de uma silhueta humana"
-            data-testid="profile-top-btn"
-          />
-        </button>
-        <h1 data-testid="page-title">Foods</h1>
-        <button
-          type="button"
-          data-testid="search-top-btn"
-          onClick={ () => setShowSearch(!showSearch) }
 
-        >
-          <img src={ imgageSearch } alt="desenho de uma lupa" />
-        </button>
+        <input
+          id="profileIcon"
+          data-testid="profile-top-btn"
+          type="image"
+          src={ profileIcon }
+          alt="desenho de uma silhueta humana"
+          onClick={ () => history.push('/profile') }
+        />
+
       </header>
-      <div>
-        <form>
-          <label htmlFor="ingredient">
-            Ingredient
-            <input
-              type="radio"
-              id="ingredient"
-              data-testid="ingredient-search-radio"
-              name="search"
-              value="ingredient"
-            />
-          </label>
-          <label htmlFor="name">
-            Name
-            <input
-              type="radio"
-              id="name"
-              data-testid="name-search-radio"
-              name="search"
-              value="name"
-            />
-          </label>
-          <label htmlFor="firstLetter">
-            First Letter
-            <input
-              type="radio"
-              id="firstLetter"
-              data-testid="first-letter-search-radio"
-              name="search"
-              value="firstLetter"
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="exec-search-btn"
-          >
-            Search
-          </button>
-          <input
-            type="text"
-            name="searchInput"
-            data-testid="search-input"
-            hidden={ showSearch }
-          />
-        </form>
-      </div>
+      <div />
     </>
   );
 };
