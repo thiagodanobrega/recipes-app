b4cd7ff547d6f752c434e76e524251954ccfea37
Thiago Nobrega <thiagons@live.com>
2022-03-30 19:00:29 -0300

    feat: implementando cards de receitas feitas

--- src/pages/DoneRecipesScreen.js
+++ src/pages/DoneRecipesScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/header';
 import useLocalStorage from '../hooks/useLocalStorage';
+import shareIcon from '../images/shareIcon.svg';
 
 function DoneRecipesScreen() {
   const [doneRecipes, setDoneRecipes] = useState([]);
@@ -38,6 +39,35 @@ function DoneRecipesScreen() {
           Drinks
         </button>
       </section>
+
+      {doneRecipes.map((recipe, index) => (
+        <div key={ index }>
+          <img
+            src={ recipe.image }
+            data-testid={ `${index}-horizontal-image` }
+            alt="Imagem da receita pronta"
+          />
+          <p
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {recipe.type === 'food'
+              ? `${recipe.nationality} ${recipe.category}` : `${recipe.alcoholicOrNot}`}
+          </p>
+          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          {recipe.type === food ? (recipe.tags.map((tag) => (
+            <p data-testid={ `${index}-${recipe.tags}-horizontal-tag` } key={ tag }>
+              {tag}
+            </p>
+          ))) : ''}
+          <img
+            src={ shareIcon }
+            data-testid={ `${index}-horizontal-share-btn` }
+            alt="Ícone de compartilhar"
+          />
+        </div>
+      ))}
+
     </div>
   );
 }
