41f2a1ba994c6c7e3000f623d381c20401b2fbc9
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 22:57:44 -0300

    feat: adiciona o header, o h1 com title correto e renderização condicional ao Explore Nationalities

--- src/pages/ExploreNationalitiesScreen.js
+++ src/pages/ExploreNationalitiesScreen.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../components/header';
+import searchIcon from '../images/searchIcon.svg';
 
 function ExploreNationalitiesScreen() {
+  const [showSearch, setShowSearch] = useState(true);
+
   return (
     <div>
-      <p>Tela de explorar comidas por nacionalidade</p>
+      <h1 data-testid="page-title">Explore Nationalities</h1>
+      <Header />
+      <form>
+        <input
+          type="image"
+          src={ searchIcon }
+          alt="desenho de uma lupa"
+          data-testid="search-top-btn"
+          onClick={ () => setShowSearch(!showSearch) }
+        />
+
+        <input
+          type="text"
+          name="searchInput"
+          data-testid="search-input"
+          hidden={ showSearch }
+        />
+      </form>
     </div>
   );
 }
