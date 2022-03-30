726116184519d191e3688fa7531a4d4b596098d8
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 23:04:05 -0300

    feat: adiciona header e h1 com title correto a Profile Screen

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/header';
 
 function ProfileScreen() {
   return (
     <div>
-      <h1>Tela de Perfil</h1>
+      <h1 data-testid="page-title">Profile</h1>
+      <Header />
     </div>
   );
 }
