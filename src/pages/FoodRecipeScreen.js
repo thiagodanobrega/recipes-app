7298bddb1afda3702af6b742bc343e13d82fd2e9
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-29 23:03:29 -0300

    feat: reorganiza estrutura da página Food Recipe Screen. add h1, radio buttons e renderização condicional do input

--- src/pages/FoodRecipeScreen.js
+++ src/pages/FoodRecipeScreen.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/header';
+import searchIcon from '../images/searchIcon.svg';
 
 function FoodRecipeScreen() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div>
       <Header />
+      <h1 data-testid="page-title">Foods</h1>
+
+      <input
+        type="image"
+        src={ searchIcon }
+        alt="desenho de uma lupa"
+        data-testid="search-top-btn"
+        onClick={ () => setShowSearch(!showSearch) }
+      />
+      {
+        showSearch && (
+          <input
+            type="text"
+            name="searchInput"
+            data-testid="search-input"
+          />
+        )
+      }
+      <form>
+
+        <label htmlFor="ingredient">
+          Ingredient
+          <input
+            type="radio"
+            id="ingredient"
+            data-testid="ingredient-search-radio"
+            name="search"
+            value="ingredient"
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="radio"
+            id="name"
+            data-testid="name-search-radio"
+            name="search"
+            value="name"
+          />
+        </label>
+        <label htmlFor="firstLetter">
+          First Letter
+          <input
+            type="radio"
+            id="firstLetter"
+            data-testid="first-letter-search-radio"
+            name="search"
+            value="firstLetter"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
