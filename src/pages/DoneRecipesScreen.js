ad8dd1b842f76b3fba1c50771c8ea92f781b6351
Thiago Nobrega <thiagons@live.com>
2022-03-30 18:26:29 -0300

    feat: Implementando botoes dos filtros

--- src/pages/DoneRecipesScreen.js
+++ src/pages/DoneRecipesScreen.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/header';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function DoneRecipesScreen() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [value] = useLocalStorage('doneRecipes', []);
+
+  useEffect(() => {
+    setDoneRecipes(value);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
-      <h1 data-testid="page-title">Done Recipes</h1>
       <Header />
+      <h1 data-testid="page-title">Done Recipes</h1>
+
+      <section>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+        >
+          Food
+        </button>
+
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </section>
     </div>
   );
 }
