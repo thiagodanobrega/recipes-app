b48b1ce992a88e23bc91eb7a77a5e0bb17b854c8
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-30 10:50:55 -0300

    build: dele form render condicional e add FormHeader

--- src/pages/ExploreNationalitiesScreen.js
+++ src/pages/ExploreNationalitiesScreen.js
@@ -1,32 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/header';
-import searchIcon from '../images/searchIcon.svg';
 import BottomMenu from '../components/BottomMenu';
+import FormHeader from '../components/formHeader';
 
 function ExploreNationalitiesScreen() {
-  const [showSearch, setShowSearch] = useState(true);
-
   return (
     <div>
       <h1 data-testid="page-title">Explore Nationalities</h1>
       <Header />
-      <form>
-        <input
-          type="image"
-          src={ searchIcon }
-          alt="desenho de uma lupa"
-          data-testid="search-top-btn"
-          onClick={ () => setShowSearch(!showSearch) }
-        />
-
-        <input
-          type="text"
-          name="searchInput"
-          data-testid="search-input"
-          hidden={ showSearch }
-        />
-      </form>
-      <p>Tela de explorar comidas por nacionalidade</p>
+      <FormHeader />
       <BottomMenu />
     </div>
   );
