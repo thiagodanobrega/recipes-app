8824f7e41611327dce74aef24d283aa3b2bf4e48
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-30 10:48:28 -0300

    build: retica nome Dink para Drinks

---
+++ src/pages/DrinkRecipeScreen.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Header from '../components/header';
+import searchIcon from '../images/searchIcon.svg';
+import BottomMenu from '../components/BottomMenu';
+
+function DrinksRecipeScreen() {
+  const [showSearch, setShowSearch] = useState(true);
+
+  return (
+    <div>
+      <h1 data-testid="page-title">Drinks</h1>
+      <Header />
+      <form>
+        <input
+          type="image"
+          src={ searchIcon }
+          alt="desenho de uma lupa"
+          data-testid="search-top-btn"
+          onClick={ () => setShowSearch(!showSearch) }
+        />
+
+        <input
+          type="text"
+          name="searchInput"
+          data-testid="search-input"
+          hidden={ showSearch }
+        />
+      </form>
+      <BottomMenu />
+    </div>
+  );
+}
+
+export default DrinksRecipeScreen;
