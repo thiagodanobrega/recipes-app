5b32772c45043dbba787596aeef38dc123a7fdad
Thiago Nobrega <thiagons@live.com>
2022-03-31 12:17:51 -0300

    feat: add botão de desfavoritar no componente CardDoneAndFavorites

--- src/components/CardDoneAndFavorites.js
+++ src/components/CardDoneAndFavorites.js
@@ -2,24 +2,27 @@ import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
-function CardDoneAndFavorites({ filterDoneRecipes }) {
+function CardDoneAndFavorites({ filterRecipes, disfavorRecipe, typeScreen }) {
   const [idCopied, setIdCopied] = useState('');
   const history = useHistory();
   const copyUrlToClipboard = (id, type) => {
     setIdCopied(id);
     // ref: https://www.w3schools.com/js/js_window_location.asp
-    const url = window.location.href.replace('done-recipes', '');
+    const url = window.location.href.replace(typeScreen === 'favorite'
+      ? 'favorite-recipes'
+      : 'done-recipes', '');
     if (type === 'food') {
       // ref: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText
       return navigator.clipboard.writeText(`${url}foods/${id}`);
     }
-    return navigator.clipboard.writeText(`${url}/drinks/${id}`);
+    return navigator.clipboard.writeText(`${url}drinks/${id}`);
   };
 
   return (
     <div>
-      {filterDoneRecipes.map((recipe, index) => (
+      {filterRecipes.map((recipe, index) => (
         <div key={ index }>
           <button
             type="button"
@@ -47,13 +50,14 @@ function CardDoneAndFavorites({ filterDoneRecipes }) {
             <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
           </Link>
 
-          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          {typeScreen === 'done'
+            && <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>}
 
-          {recipe.type === 'food' ? (recipe.tags.map((tag) => (
+          {typeScreen === 'done' && (recipe.tags.map((tag) => (
             <p data-testid={ `${index}-${tag}-horizontal-tag` } key={ tag }>
               {tag}
             </p>
-          ))) : ''}
+          ))) }
 
           <input
             type="image"
@@ -64,6 +68,15 @@ function CardDoneAndFavorites({ filterDoneRecipes }) {
           />
 
           {idCopied === recipe.id && <p>Link copied!</p>}
+
+          {typeScreen === 'favorite'
+            && <input
+              type="image"
+              src={ blackHeartIcon }
+              alt="Ícone de coração para favoritar"
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              onClick={ () => disfavorRecipe(recipe.id) }
+            />}
         </div>
       ))}
     </div>
@@ -71,7 +84,9 @@ function CardDoneAndFavorites({ filterDoneRecipes }) {
 }
 
 CardDoneAndFavorites.propTypes = {
-  filterDoneRecipes: PropTypes.func.isRequired,
+  filterRecipes: PropTypes.func.isRequired,
+  disfavorRecipe: PropTypes.func.isRequired,
+  typeScreen: PropTypes.string.isRequired,
 };
 
 export default CardDoneAndFavorites;
