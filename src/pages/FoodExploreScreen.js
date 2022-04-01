d41b8cb6f4e5e15544f5fbb1f368e2506cc6faf6
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-01 12:00:54 -0300

    build:  substitui componente Header por ProfilePicture

--- src/pages/FoodExploreScreen.js
+++ src/pages/FoodExploreScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from '../components/header';
 import BottomMenu from '../components/BottomMenu';
+import ProfilePicture from '../components/Header/ProfilePicture';
 
 function FoodExploreScreen() {
   return (
     <div>
       <h1 data-testid="page-title">Explore Foods</h1>
-      <Header />
+      <ProfilePicture />
       <p>Tela de explorar comidas</p>
       <BottomMenu />
     </div>
