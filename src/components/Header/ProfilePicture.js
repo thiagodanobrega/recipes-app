a89d8dc9bb065d811330ee902d5a17410969de6e
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-01 11:56:27 -0300

    feat: cria file ProfilePicture

---
+++ src/components/Header/ProfilePicture.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../../images/profileIcon.svg';
+
+const ProfilePicture = () => {
+  const history = useHistory();
+  return (
+    <>
+      <header>
+
+        <input
+          id="profileIcon"
+          data-testid="profile-top-btn"
+          type="image"
+          src={ profileIcon }
+          alt="desenho de uma silhueta humana"
+          onClick={ () => history.push('/profile') }
+        />
+
+      </header>
+      <div />
+    </>
+  );
+};
+
+export default ProfilePicture;
