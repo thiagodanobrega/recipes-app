ba184df4159131325663eb4f634f9cf401aa273f
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-01 12:14:52 -0300

    troca as requisiçoes de lugar

--- src/context/contextFoodRecipe/ProviderFoods.js
+++ src/context/contextFoodRecipe/ProviderFoods.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ContextFoodRecipe from './contextFoodRecipe';
 
 function ProviderFoods({ children }) {
@@ -12,53 +12,6 @@ function ProviderFoods({ children }) {
   // estado das buscas do user - tipo e textos
   const [userChoiceTypeSearch, setUserChoiceTypeSearch] = useState('');
   const [userTypedText, setUserTypedText] = useState('');
-
-  // requisição de ingredientes
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
-      const dataIngredients = await response.json();
-      setIngredients(dataIngredients.meals);
-    })();
-  }, []);
-
-  // requisição de categorias
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
-      const dataCategories = await response.json();
-      setCategories(dataCategories.meals);
-    })();
-  }, []);
-
-  // requisição de nationalities
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(' https://www.themealdb.com/api/json/v1/1/list.php?a=list');
-      const nationality = await response.json();
-      setNationalities(nationality.meals);
-    })();
-  }, []);
-
-  // requisição de first letters
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
-      const firstLetterData = await response.json();
-      setFirstLetter(firstLetterData.meals);
-    })();
-  }, []);
-
-  // requisição por nome
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`www.themealdb
-      .com/api/json/v1/1/search.php?s=Arrabiata`);
-      const nameData = await response.json();
-      console.log('loguei Name', nameData);
-      setNameMeals(nameData.meals);
-    })();
-  }, []);
 
   const contextValue = {
     ingredients,
--- src/pages/FoodRecipeScreen.js
+++ src/pages/FoodRecipeScreen.js
@@ -9,6 +9,53 @@ function FoodRecipeScreen() {
   const { setUserChoiceTypeSearch } = useContext(contextFoodRecipes);
   const [userChosetUserChoiceceOnclick, setUserChoiceOnclick] = useState('');
 
+  // requisição de ingredientes
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+      const dataIngredients = await response.json();
+      setIngredients(dataIngredients.meals);
+    })();
+  }, []);
+
+  // requisição de categorias
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      const dataCategories = await response.json();
+      setCategories(dataCategories.meals);
+    })();
+  }, []);
+
+  // requisição de nationalities
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(' https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+      const nationality = await response.json();
+      setNationalities(nationality.meals);
+    })();
+  }, []);
+
+  // requisição de first letters
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
+      const firstLetterData = await response.json();
+      setFirstLetter(firstLetterData.meals);
+    })();
+  }, []);
+
+  // requisição por nome
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`www.themealdb
+      .com/api/json/v1/1/search.php?s=Arrabiata`);
+      const nameData = await response.json();
+      console.log('loguei Name', nameData);
+      setNameMeals(nameData.meals);
+    })();
+  }, []);
+
   const submitRadioValue = (e) => {
     e.preventDefault();
     setUserChoiceTypeSearch(userChosetUserChoiceceOnclick);
