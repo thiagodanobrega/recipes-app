6fd07d21d8322990f6779c10d74696786b8b88d3
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-01 12:01:59 -0300

    build:  substitui componente Header por ProfilePicture

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Header from '../components/header';
 import BottomMenu from '../components/BottomMenu';
 
 function ProfileScreen() {
   return (
     <div>
       <h1 data-testid="page-title">Profile</h1>
-      <Header />
+      <ProfileScreen />
       <h1>Tela de Perfil</h1>
       <BottomMenu />
     </div>
