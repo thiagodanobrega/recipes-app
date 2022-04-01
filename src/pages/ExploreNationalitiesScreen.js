0e3a42ea35ae152c81ac6411a2cd16f890ba3dbc
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-01 12:00:15 -0300

    build:  substitui componente Header por ProfilePicture e formHeader por InputText

--- src/pages/ExploreNationalitiesScreen.js
+++ src/pages/ExploreNationalitiesScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Header from '../components/header';
 import BottomMenu from '../components/BottomMenu';
-import FormHeader from '../components/formHeader';
+import InputText from '../components/Header/InputText';
+import ProfilePicture from '../components/Header/ProfilePicture';
 
 function ExploreNationalitiesScreen() {
   return (
     <div>
       <h1 data-testid="page-title">Explore Nationalities</h1>
-      <Header />
-      <FormHeader />
+      <ProfilePicture />
+      <InputText />
       <BottomMenu />
     </div>
   );
