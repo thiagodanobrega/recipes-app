50b6910f7d52e08a9535f4b1f97a373d6967251a
Thiago Nobrega <thiagons@live.com>
2022-04-02 11:45:02 -0300

    refactor: refatorando tela de food

--- src/pages/FoodRecipeScreen.js
+++ src/pages/FoodRecipeScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ProfilePicture from '../components/Header/ProfilePicture';
+import Header from '../components/Header/Header';
 import BottomMenu from '../components/BottomMenu';
-import InputSearchBar from '../components/Header/InputSearchBar';
 
 function FoodRecipeScreen() {
   return (
     <div>
-      <ProfilePicture />
-      <h1 data-testid="page-title">Foods</h1>
-      <InputSearchBar />
+      <Header
+        renderScreen
+        nameScreen="Foods"
+        dataTest="page-title"
+      />
       <BottomMenu />
     </div>
   );
