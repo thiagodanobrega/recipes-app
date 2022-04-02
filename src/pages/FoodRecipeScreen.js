53a77e4752d2731f8ac6f43e89276b409f75c8ff
Thiago Nobrega <thiagons@live.com>
2022-04-02 14:40:09 -0300

    feat: comentando componente Card

--- src/pages/FoodRecipeScreen.js
+++ src/pages/FoodRecipeScreen.js
@@ -3,18 +3,29 @@ import Header from '../components/Header/Header';
 import BottomMenu from '../components/BottomMenu';
 
 function FoodRecipeScreen() {
+  // const MAX_INGREDIENTS = 12;
   return (
     <div>
       <Header
         renderScreen
         nameScreen="Foods"
       />
-      <Card
-        name={ obj.strMeal }
-        image={ obj.strMealThumb }
-        typeCard="recipe-card"
-        index={ index }
-      />
+
+      {/* <section>
+        {array.slice(0, MAX_INGREDIENTS).map((obj, index) => (
+          <button
+            type="button"
+            key={ index }
+          >
+            <Card
+              name={ obj.strMeal }
+              image={ obj.strMealThumb }
+              typeCard="recipe-card"
+              index={ index }
+            />
+          </button>
+        ))}
+      </section> */}
       <BottomMenu />
     </div>
   );
