4b78e9177366808348a144884f9ffe51ec564c84
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-03 19:25:32 -0300

    build: adiciona categoryDrinks no estdo inicial para pesquisa  do usuário para fazser a chamada da API de acordo com a escolha da categoria do usuário

--- src/context/contextDrinks/ProviderDrinks.js
+++ src/context/contextDrinks/ProviderDrinks.js
@@ -7,55 +7,68 @@ function ProviderDrinks({ children }) {
   const USER_INITIAL_STATE = {
     typeSearch: '',
     textSearch: '',
+    categoryDrinks: '',
   };
   // estado das requisições DRINKS
-  const [drinks, setDrinks] = useState([]);
+  const [allDrinksData, setAllDrinksData] = useState([]);// todas bebidas renderizadas por nome
+  const [error, setError] = useState('');
+  const [drinks, setDrinks] = useState([]);// lista de bebidas escolhidas pelo usuário
 
-  // estado das escolhas do usuário tipo e texto
-  const [userChoice, setUserChoice] = useState(USER_INITIAL_STATE);
+  // estado das escolhas do usuário tipo de pesquisa, texto do input e categoria
+  const [userChoiceDrinks, setUserChoiceDrinks] = useState(USER_INITIAL_STATE);
 
-  // chamada api
+  // callAPi chamada api de acordo com o tipo de pesquisa do usuario
   const [callApi, setCallApi] = useState('');
 
   // ---------------------ENDPOINTS--------------------------------
+  const { categoryDrinks, textSearch, typeSearch } = userChoiceDrinks;
+  if (categoryDrinks) {
+    const CATEGORY = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoryDrinks}`;
+    setCallApi(CATEGORY);
+    setUserChoiceDrinks(USER_INITIAL_STATE);
+  }
 
-  if (userChoice.typeSearch === 'ingredient') {
-    const DRINKS_INGREDIENT_API = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${userChoice.textSearch}`;
+  if (typeSearch === 'ingredient') {
+    const DRINKS_INGREDIENT_API = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${textSearch}`;
     setCallApi(DRINKS_INGREDIENT_API);
-    setUserChoice({
-      typeSearch: '',
-      textSearch: '',
-    });
+    setUserChoiceDrinks(USER_INITIAL_STATE);
   }
 
-  if (userChoice.typeSearch === 'name') {
-    const DRINKS_NAME_API = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userChoice.textSearch}`;
+  if (typeSearch === 'name') {
+    const DRINKS_NAME_API = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${textSearch}`;
     setCallApi(DRINKS_NAME_API);
-    setUserChoice({
-      typeSearch: '',
-      textSearch: '',
-    });
+    setUserChoiceDrinks(USER_INITIAL_STATE);
   }
 
-  if (userChoice.typeSearch === 'firstLetter') {
-    const DRINKS_FIRST_LETTER_API = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${userChoice.textSearch}`;
+  if (typeSearch === 'firstLetter') {
+    const DRINKS_FIRST_LETTER_API = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${textSearch}`;
     setCallApi(DRINKS_FIRST_LETTER_API);
-    setUserChoice({
-      typeSearch: '',
-      textSearch: '',
-    });
+    setUserChoiceDrinks(USER_INITIAL_STATE);
   }
   const { data, isLoading } = useFetch(callApi);
-
   useEffect(() => {
     if (data) { setDrinks(data.drinks); }
   }, [data]);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        const dataAPI = await response.json();
+        setAllDrinksData(dataAPI.drinks);
+      } catch (erro) {
+        setError(erro.message);
+      }
+    })();
+  }, []);
+
   const contextValue = {
+    allDrinksData,
+    error,
     drinks,
     setDrinks,
-    userChoice,
-    setUserChoice,
+    userChoiceDrinks,
+    setUserChoiceDrinks,
     isLoading,
   };
 
