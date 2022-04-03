8a0a575486c5fc1e09d83e9ad0de98787756bdf2
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-02 22:01:16 -0300

    feat: estrutando provider Drinks

--- src/context/contextDrinks/ProviderDrinks.js
+++ src/context/contextDrinks/ProviderDrinks.js
@@ -1,21 +1,63 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContextDrinks from './contextDrinks';
+import useFetch from '../../hooks/useFetch';
 
 function ProviderDrinks({ children }) {
+  const USER_INITIAL_STATE = {
+    typeSearch: '',
+    textSearch: '',
+  };
   // estado das requisições DRINKS
-  const [drinksIngredients, setDrinksIngredients] = useState([]);
-  const [drinksNameMeals, setDrinksNameMeals] = useState([]);
-  const [drinksFirstLetter, setDrinksFirstLetter] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  // estado das escolhas do usuário tipo e texto
+  const [userChoice, setUserChoice] = useState(USER_INITIAL_STATE);
+
+  // chamada api
+  const [callApi, setCallApi] = useState('');
+
+  // ---------------------ENDPOINTS--------------------------------
+
+  if (userChoice.typeSearch === 'ingredient') {
+    const DRINKS_INGREDIENT_API = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${userChoice.textSearch}`;
+    setCallApi(DRINKS_INGREDIENT_API);
+    setUserChoice({
+      typeSearch: '',
+      textSearch: '',
+    });
+  }
+
+  if (userChoice.typeSearch === 'name') {
+    const DRINKS_NAME_API = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userChoice.textSearch}`;
+    setCallApi(DRINKS_NAME_API);
+    setUserChoice({
+      typeSearch: '',
+      textSearch: '',
+    });
+  }
+
+  if (userChoice.typeSearch === 'firstLetter') {
+    const DRINKS_FIRST_LETTER_API = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${userChoice.textSearch}`;
+    setCallApi(DRINKS_FIRST_LETTER_API);
+    setUserChoice({
+      typeSearch: '',
+      textSearch: '',
+    });
+  }
+  const { data } = useFetch(callApi);
+
+  useEffect(() => {
+    if (data) { setDrinks(data.drinks); }
+  }, [data]);
 
   const contextValue = {
-    drinksIngredients,
-    setDrinksIngredients,
-    drinksNameMeals,
-    setDrinksNameMeals,
-    drinksFirstLetter,
-    setDrinksFirstLetter,
+    drinks,
+    setDrinks,
+    userChoice,
+    setUserChoice,
   };
+
   return (
     <ContextDrinks.Provider value={ contextValue }>
       {children}
