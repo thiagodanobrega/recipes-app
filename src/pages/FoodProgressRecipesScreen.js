637d074edba40365bd58b4051aa477aa690da45f
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-04 10:28:37 -0300

    Feat: Tratando o data e renderizando ingredientes

--- src/pages/FoodProgressRecipesScreen.js
+++ src/pages/FoodProgressRecipesScreen.js
@@ -10,6 +10,38 @@ function FoodProgressRecipesScreen() {
   const endPointFood = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
   const { data } = useFetch(endPointFood);
 
+  const renderIngredients = () => {
+    if (data) {
+      const arrAllKeysWithValue = data.meals.map((element) => Object.keys(element)
+        .filter((key) => element[key] !== '' && element[key] !== null));
+
+      const ingredient = arrAllKeysWithValue[0]
+        .filter((element) => element.includes('strIngredient'));
+
+      const measure = arrAllKeysWithValue[0].filter((key) => key.includes('strMeasure'));
+      // Percorrendo o array das chaves e acessando elas para obter os valores no array original.
+      const ingredientsValues = ingredient.map((key) => data.meals[0][key]);
+      const measureValues = measure.map((key) => data.meals[0][key]);
+
+      return (
+        ingredientsValues.map((ingredientValue, index) => (
+          <label
+            data-testid={ `${index}-ingredient-step` }
+            htmlFor={ ingredientValue }
+            key={ ingredientValue }
+          >
+            <input
+              id={ ingredientValue }
+              type="checkbox"
+              onClick={ (event) => changeTargetStyle(event) }
+            />
+            { `${ingredientValue} - ${measureValues[index]}` }
+          </label>
+        ))
+      );
+    }
+  };
+
   return (
     <div>
       <p>Tela de receita em progresso de comida</p>
