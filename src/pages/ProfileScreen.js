3c51dd2dfa761db68a790d5d8dc6246f866cf4c4
Thiago Nobrega <thiagons@live.com>
2022-04-04 16:59:55 -0300

    feat: implementando elementos e atributos

--- src/pages/ProfileScreen.js
+++ src/pages/ProfileScreen.js
@@ -4,13 +4,34 @@ import BottomMenu from '../components/BottomMenu';
 
 function ProfileScreen() {
   return (
-    <div>
+    <>
       <Header
         renderScreen={ false }
         nameScreen="Profile"
       />
+      <main>
+        <p data-testid="profile-email" />
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </main>
       <BottomMenu />
-    </div>
+    </>
   );
 }
 
