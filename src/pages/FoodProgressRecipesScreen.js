e6c5a764e2575890f711986a6067578090346793
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-04 10:29:55 -0300

    Feat: Renderizando os itens

--- src/pages/FoodProgressRecipesScreen.js
+++ src/pages/FoodProgressRecipesScreen.js
@@ -42,9 +42,30 @@ function FoodProgressRecipesScreen() {
     }
   };
 
+  if (!data) return 'carregando...';
   return (
     <div>
-      <p>Tela de receita em progresso de comida</p>
+      <img
+        data-testid="recipe-photo"
+        src={ data.meals[0].strMealThumb }
+        alt="imagem da receita"
+      />
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favoritar</button>
+      <h1 data-testid="recipe-title">{ data.meals[0].strMeal }</h1>
+      <p data-testid="recipe-category">{ data.meals[0].strCategory }</p>
+      <h3>Ingredientes:</h3>
+      { renderIngredients() }
+      <h3>Instruções:</h3>
+      <p data-testid="instructions">
+        { data.meals[0].strInstructions }
+      </p>
+      <button
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Finalizar Receita
+      </button>
     </div>
   );
 }
