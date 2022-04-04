e12f4411247a5f7f09125f54b837f08533bd3921
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-04 10:27:25 -0300

    Feat: Criando a requisição.

--- src/pages/FoodProgressRecipesScreen.js
+++ src/pages/FoodProgressRecipesScreen.js
@@ -4,6 +4,12 @@ import { useHistory } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 function FoodProgressRecipesScreen() {
+  const history = useHistory();
+  // const { id } = useParams();
+  const id = 52772;
+  const endPointFood = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const { data } = useFetch(endPointFood);
+
   return (
     <div>
       <p>Tela de receita em progresso de comida</p>
