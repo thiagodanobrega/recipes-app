7b434e00e95af23499172746655d5ce5d2326dc6
Thiago Nobrega <thiagons@live.com>
2022-04-05 09:46:12 -0300

    feat: tela de ingredients

--- src/pages/ExploreDrinkIngredientsScreen.js
+++ src/pages/ExploreDrinkIngredientsScreen.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import BottomMenu from '../components/BottomMenu';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import BottomMenu from '../components/BottomMenu';
+import Card from '../components/Card';
+import contextDrinks from '../context/contextDrinks/contextDrinks';
+import useFetch from '../hooks/useFetch';
 
-function ExploreDrinkIngredientsScreen() {
+function ExploreFoodIngredientsScreen() {
+  const { setDrinks } = useContext(contextDrinks);
+  const MAX_INGREDIENTS = 12;
+  const history = useHistory();
+  const { data } = useFetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setValue(data.drinks);
+      console.log(data.drinks);
+    }
+  }, [data]);
+
+  const handleButton = async (name) => {
+    const ENDPOINT = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`;
+    const response = await fetch(ENDPOINT);
+    const dataValue = await response.json();
+    setDrinks(dataValue.drinks);
+    history.push('/drinks');
+  };
+
   return (
-    <div>
+    <>
       <Header
         renderScreen={ false }
         nameScreen="Explore Ingredients"
       />
+      <section>
+        {value.slice(0, MAX_INGREDIENTS).map((obj, index) => (
+          <button
+            type="button"
+            key={ index }
+            onClick={ () => handleButton(obj.strIngredient1) }
+          >
+            <Card
+              name={ obj.strIngredient1 }
+              image={
+                `https://www.thecocktaildb.com/images/ingredients/${obj.strIngredient1}-Small.png`
+              }
+              typeCard="ingredient-card"
+              index={ index }
+            />
+          </button>
+        ))}
+      </section>
       <BottomMenu />
-    </div>
+    </>
   );
 }
 
-export default ExploreDrinkIngredientsScreen;
+export default ExploreFoodIngredientsScreen;
--- src/pages/ExploreFoodIngredientsScreen.js
+++ src/pages/ExploreFoodIngredientsScreen.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import BottomMenu from '../components/BottomMenu';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import BottomMenu from '../components/BottomMenu';
+import Card from '../components/Card';
+import contextFoodRecipe from '../context/contextFoodRecipe/contextFoodRecipe';
+import useFetch from '../hooks/useFetch';
 
 function ExploreFoodIngredientsScreen() {
+  const { setFoods } = useContext(contextFoodRecipe);
+  const MAX_INGREDIENTS = 12;
+  const history = useHistory();
+  const { data } = useFetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setValue(data.meals);
+      console.log(data.meals);
+    }
+  }, [data]);
+
+  const handleButton = async (name) => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`);
+    const dataValue = await response.json();
+    setFoods(dataValue.meals);
+    history.push('/foods');
+  };
+
   return (
-    <div>
+    <>
       <Header
         renderScreen={ false }
         nameScreen="Explore Ingredients"
       />
+      <section>
+        {value.slice(0, MAX_INGREDIENTS).map((obj, index) => (
+          <button
+            type="button"
+            key={ index }
+            onClick={ () => handleButton(obj.strIngredient) }
+          >
+            <Card
+              name={ obj.strIngredient }
+              image={ `https://www.themealdb.com/images/ingredients/${obj.strIngredient}-Small.png` }
+              typeCard="ingredient-card"
+              index={ index }
+            />
+          </button>
+        ))}
+      </section>
       <BottomMenu />
-    </div>
+    </>
   );
 }
 
