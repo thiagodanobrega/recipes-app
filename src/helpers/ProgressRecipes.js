a5d6936a56fc38595bdd7dd3ff2ef5951fb252f2
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-05 13:58:44 -0300

    Fix: Complexidade

---
+++ src/helpers/ProgressRecipes.js
@@ -0,0 +1,19 @@
+import { useHistory } from 'react-router-dom';
+
+const handleButton = () => {
+  // const storage = JSON.parse(localStorage
+  //   .getItem('inProgressRecipes')).meals[id];
+
+  // if (storage && lucas.length === renderIngredients().length) {
+  //   setEnabledButton(true);
+    history.push('/done-recipes');
+  }
+};
+
+// ProgressRecipes.propTypes = {
+//   changeTargetStyle: PropTypes.func.isRequired,
+//   data: PropTypes.shape({
+//     meals: PropTypes.arrayOf.isRequired,
+//   }).isRequired,
+// };
+export default handleButton;
