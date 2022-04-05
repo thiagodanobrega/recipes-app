37d3871c63da860c2353b98a7dd8759a2394e7b0
(GJ) Gabriel Julio <89492349+GJTrybe@users.noreply.github.com>
2022-04-05 12:22:14 -0300

    cria componente de video embed

---
+++ src/components/EmbedVideo.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const EmbedVideo = ({ embedId }) => (
+  <iframe
+    width="340"
+    height="160"
+    src={ `https://www.youtube.com/embed/${embedId}` }
+    frameBorder="0"
+    allow="accelerometer;
+      autoplay;
+      clipboard-write;
+      encrypted-media;
+      gyroscope;
+      picture-in-picture"
+    allowFullScreen
+    title="EmbedVideo"
+  />
+);
+
+EmbedVideo.propTypes = {
+  embedId: PropTypes.string.isRequired,
+};
+
+export default EmbedVideo;
