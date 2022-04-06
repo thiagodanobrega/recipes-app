c8a8fb2de514736efafa1db1a6cb271294d6dfa6
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-06 16:35:09 -0300

    fix: lint

--- src/helpers/listIngredientsAndMeasures.js
+++ src/helpers/listIngredientsAndMeasures.js
@@ -1,21 +1,21 @@
 const renderIngredients = (data) => {
   const arrIngredientMeasure = [];
-  const arrAllKeysWithValue = data.meals
-    .map((element) => Object.keys(element)
-      .filter((key) => element[key] !== '' && element[key] !== null));
+
+  const arrAllKeysWithValue = data.meals.map((element) => Object.keys(element)
+    .filter((key) => element[key] !== '' && element[key] !== null));
+
   const ingredient = arrAllKeysWithValue[0]
     .filter((element) => element.includes('strIngredient'));
-  const measure = arrAllKeysWithValue[0]
-    .filter((key) => key
-      .includes('strMeasure'));
-  const ingredientsValues = ingredient
-    .map((key) => data.meals[0][key]);
-  const measureValues = measure
-    .map((key) => data.meals[0][key]);
-  ingredientsValues
-    .map((ingredientValue, index) => (arrIngredientMeasure
-      .push(`${ingredientValue} - ${measureValues[index]}`)
-    ));
+
+  const measure = arrAllKeysWithValue[0].filter((key) => key.includes('strMeasure'));
+
+  const ingredientsValues = ingredient.map((key) => data.meals[0][key]);
+
+  const measureValues = measure.map((key) => data.meals[0][key]);
+
+  ingredientsValues.map((ingredientValue, index) => (arrIngredientMeasure
+    .push(`${ingredientValue} - ${measureValues[index]}`)
+  ));
   return (arrIngredientMeasure);
 };
 
