c8dd7564ce3e41ea7c63b05c70235faef49c5557
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-07 19:03:41 -0300

    Refactor: solução para erro do checked

--- src/helpers/getCheckedFoods.js
+++ src/helpers/getCheckedFoods.js
@@ -1,7 +1,17 @@
+// const getChecked = (ingredients, id) => {
+//   const inProgressKey = localStorage.getItem('inProgressRecipes');
+//   if (inProgressKey) {
+//     return JSON.parse(inProgressKey).meals[id].includes(+ingredients);
+//   }
+// };
+
+// export default getChecked;
 const getChecked = (ingredients, id) => {
   const inProgressKey = localStorage.getItem('inProgressRecipes');
   if (inProgressKey) {
-    return JSON.parse(inProgressKey).meals[id].includes(ingredients);
+    const ingName = ingredients.split('-')[0];
+    const ingred = new RegExp(ingName, 'ig');
+    return JSON.parse(inProgressKey).meals[id].some((item) => ingred.test(item));
   }
 };
 
