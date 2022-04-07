9ef0aded4b737cdb15168c67d9b2e0c2d1d3d85a
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-04-07 19:04:06 -0300

    Fix: lint

--- src/helpers/getCheckedDrinks.js
+++ src/helpers/getCheckedDrinks.js
@@ -1,13 +1,9 @@
 const getChecked = (ingredients, id) => {
   const inProgressKey = localStorage.getItem('inProgressRecipes');
-  //   console.log('localStorage key', JSON.parse(inProgressKey));
-  //   console.log('localStorage key', JSON.parse(inProgressKey)
-  //     .cocktails[id].includes(ingredients));
-  //   console.log('localStorage Ingredientes', ingredients);
   if (inProgressKey) {
-    console.log('localStorage key', JSON.parse(inProgressKey)
-      .cocktails[id].includes(ingredients));
-    return JSON.parse(inProgressKey).cocktails[id].includes(ingredients);
+    const ingName = ingredients.split('-')[0];
+    const ingred = new RegExp(ingName, 'ig');
+    return JSON.parse(inProgressKey).meals[id].some((item) => ingred.test(item));
   }
 };
 
