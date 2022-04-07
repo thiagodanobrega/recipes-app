9a75cf71b982970600ac1212f01502e1b5eef010
(GJ) Gabriel Julio <89492349+GJTrybe@users.noreply.github.com>
2022-04-07 02:29:14 -0300

    cria tela que recomenda drinks

---
+++ src/components/DrinksRecommended.js
@@ -0,0 +1,44 @@
+import { React, useContext } from 'react';
+import '../App.css';
+import ContextDrinks from '../context/contextDrinks/contextDrinks';
+import Loading from './Loading';
+
+const MAX_RECIPES = 6;
+
+const DrinksRecommended = () => {
+  const {
+    allDrinksData,
+    isLoading,
+  } = useContext(ContextDrinks);
+
+  if (isLoading || !allDrinksData) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="gallery_scroller">
+      {
+        allDrinksData.slice(0, MAX_RECIPES).map((drink, index) => (
+          <div
+            data-testid={ `${index}-recomendation-card` }
+            key={ drink.strDrink }
+          >
+            <img
+              src={ drink.strDrinkThumb }
+              alt={ drink.strDrink }
+            />
+            <span>
+              {drink.strCategory}
+            </span>
+            <h3
+              data-testid={ `${index}-recomendation-title` }
+            >
+              {drink.strDrink}
+            </h3>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+export default DrinksRecommended;
