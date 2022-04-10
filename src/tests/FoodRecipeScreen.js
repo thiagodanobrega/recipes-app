ec796d900eaaf9066e39dcdd548d268b00ef618e
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-10 17:57:39 -0300

    test: inicia  testes da pagina FoodRecipeScreen

---
+++ src/tests/FoodRecipeScreen.js
@@ -0,0 +1,18 @@
+/* import React from 'react';
+import userEvent from '@testing-library/user-event';
+
+import FoodRecipeScreen from '../pages/FoodRecipeScreen';
+import renderWithRouter from '../helpers/renderWithRouter';
+import ProviderFoods from '../context/contextFoodRecipe/ProviderFoods';
+import ProviderDrinks from '../context/contextDrinks/ProviderDrinks';
+import meals from '../../cypress/mocks/meals';
+import drinks from '../../cypress/mocks/drinks';
+import DrinksRecipeScreen from '../pages/DrinkRecipeScreen';
+
+const { screen } = require('@testing-library/react');
+
+const searchButtonTestID = 'search-top-btn';
+const profileButtonTestID = 'profile-top-btn';
+afterEach(() => jest.clearAllMocks());
+
+ */
