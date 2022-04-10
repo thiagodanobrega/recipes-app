f41c6dbb9df87987e62ba78876aa223606df4542
Suelen Arruda <suelenonyrius@gmail.com>
2022-04-10 17:53:05 -0300

    test: importa drinksRecipeScreen a Header.test

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -7,6 +7,7 @@ import ProviderFoods from '../context/contextFoodRecipe/ProviderFoods';
 import ProviderDrinks from '../context/contextDrinks/ProviderDrinks';
 import meals from '../../cypress/mocks/meals';
 import drinks from '../../cypress/mocks/drinks';
+import DrinksRecipeScreen from '../pages/DrinkRecipeScreen';
 
 const { screen } = require('@testing-library/react');
 
